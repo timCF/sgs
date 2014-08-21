6ef0c7c0dcfc89cfcb70d93ec35985fc70cec271
tim <llStrainll@gmail.com>
2014-08-21 22:53:43 +0400

    some fixes

--- lib/compiletest.ex
+++ lib/compiletest.ex
@@ -4,6 +4,13 @@ defmodule CompileTest do
 	
 	@timeout :timer.seconds(10)
 
+	init_sgs state: :not_found, nameproc: name do
+		IO.puts "HELLO, #{name}!"
+		IO.puts "Can't found your state :("
+		IO.puts "Set it 0"
+		{:ok , 0, @timeout}
+	end
+
 	init_sgs state: state, nameproc: name, when: (name == :myself) do
 		IO.puts "HELLO, here i am!"
 		IO.puts "Init state is #{inspect state}"
@@ -30,11 +37,34 @@ defmodule CompileTest do
 		{:reply, state+10, state+10, @timeout}
 	end
 
-	info_sgs :timeout, state: some_state do
+	info_sgs :timeout, state: some_state = 3 do
+		IO.inspect 3
 		IO.puts "timeout!"
 		IO.puts "auto increment of state: #{some_state+1}"
 		{:noreply, some_state+1, @timeout}
 	end
+
+	info_sgs :timeout, state: some_state = 2 do
+		IO.inspect 2
+		IO.puts "timeout!"
+		IO.puts "auto increment of state: #{some_state+1}"
+		{:noreply, some_state+1, @timeout}
+	end
+
+	info_sgs :timeout, state: some_state = 0 do
+		IO.inspect 0
+		IO.puts "timeout!"
+		IO.puts "auto increment of state: #{some_state+1}"
+		{:noreply, some_state+1, @timeout}
+	end
+
+	info_sgs :timeout, state: some_state = 1 do
+		IO.inspect 1
+		IO.puts "timeout!"
+		IO.puts "auto increment of state: #{some_state+1}"
+		{:noreply, some_state+1, @timeout}
+	end
+
 
 	terminate_sgs reason: reason, state: state do
 		IO.puts "Terminating becouse of reason #{inspect reason}, when state was #{inspect state}"
--- lib/macro.ex
+++ lib/macro.ex
@@ -2,7 +2,24 @@
 
 defmodule Sgs.Macro do
 
+	#def priv_funcs_writer collected \\ (quote do end) do
+	#	receive do
+	#		%{ quoted_to_append: incoming } -> 	priv_funcs_writer( quote do  
+	#														unquote(collected)
+	#														unquote(incoming)
+	#													end )
+	#		%{ give_result_to: pid } -> send( pid, %{ quoted_result: collected } )
+	#									:pg2.leave( "priv_funcs_writer", self )
+	#	end
+	#end
+
 	defmacro __using__(_) do
+
+		#:pg2.create "priv_funcs_writer"
+		#
+		#pid = spawn_link( Sgs.Macro, :priv_funcs_writer, [quote do end] )
+		#:pg2.join( "priv_funcs_writer", pid )
+
 		quote do 
 			use ExActor.GenServer
 			use Sgs
@@ -95,10 +112,7 @@ defmodule Sgs.Macro do
 	              			terminate_was_called: false }
               			end
 
-		quote do
-			# notice, GS is named, name !!is atom!! 
-			definit name, when: is_atom(name), do: ( definit_body( name, Exdk.get(name)) )
-
+        priv_function_body = quote do
 			defp definit_body( 	unquote(do_pattern_matching( quote do __nameproc__ end, __nameproc__ )),
 								unquote(do_pattern_matching( quote do __state__ end, __state__)) ) when unquote(make_guard(__guard__)) do
 				:erlang.register(__nameproc__, self())
@@ -110,22 +124,31 @@ defmodule Sgs.Macro do
 				init_return( unquote(body), __nameproc__ )
 			end
 		end
+
+        #send :pg2.get_members("priv_funcs_writer")
+		#		|> List.first, %{quoted_to_append: priv_function_body}
+
+		quote do
+			# notice, GS is named, name !!is atom!! 
+			definit name, when: is_atom(name), do: ( definit_body( name, Exdk.get(name)) )
+			unquote(priv_function_body)
+		end
 	end
 
 	defmacro cast_sgs(funcdef, opts \\ [], [do: body]) do
+
 
 		__nameproc__ = opts[:nameproc]
 		__state__ = opts[:state]
 		__guard__ = opts[:when]
 
-		quoted_body = quote do
+		priv_function_body = quote do
 			defp defcast_body( 	unquote(do_pattern_matching( quote do __nameproc__ end, __nameproc__ )),
 								unquote(do_pattern_matching( quote do __state__ end, __state__)) ) when unquote(make_guard(__guard__)) do
 
 				cast_info_return(unquote(body), __nameproc__) # cast and info return values are the same
 			end
-		end
-		|> insert_user_args(funcdef)
+		end |> insert_user_args(funcdef)
 
 		quoted_func_call = quote do defcast_body( name, Exdk.get(name)) end |> insert_user_args_fo_func_call(funcdef)
 
@@ -133,24 +156,24 @@ defmodule Sgs.Macro do
 			defcast	unquote(funcdef), state: name do
 				unquote(quoted_func_call)
 			end
-			unquote(quoted_body)
+			unquote(priv_function_body)
 		end
 
 	end
 
 	defmacro call_sgs(funcdef, opts \\ [], [do: body]) do
 
+
 		__nameproc__ = opts[:nameproc]
 		__state__ = opts[:state]
 		__guard__ = opts[:when]
 
-		quoted_body = quote do
+		priv_function_body = quote do
 			defp defcall_body( 	unquote(do_pattern_matching( quote do __nameproc__ end, __nameproc__ )),
 								unquote(do_pattern_matching( quote do __state__ end, __state__)) ) when unquote(make_guard(__guard__)) do
 				call_return(unquote(body), __nameproc__)
 			end
-		end
-		|> insert_user_args(funcdef)
+		end |> insert_user_args(funcdef)
 
 		quoted_func_call = quote do defcall_body(name, Exdk.get(name)) end |> insert_user_args_fo_func_call(funcdef)
 
@@ -158,7 +181,7 @@ defmodule Sgs.Macro do
 			defcall	unquote(funcdef), state: name do
 				unquote(quoted_func_call)
 			end
-			unquote(quoted_body)
+			unquote(priv_function_body)
 		end
 
 	end
@@ -169,19 +192,19 @@ defmodule Sgs.Macro do
 		__state__ = opts[:state]
 		__guard__ = opts[:when]
 
-
-		quote do
-
-			definfo	unquote(some), state: name do
-				definfo_body(name, Exdk.get(name), unquote(some))
-			end
-
+		priv_function_body = quote do
 			defp definfo_body( 	unquote(do_pattern_matching( quote do __nameproc__ end, __nameproc__ )),
 								unquote(do_pattern_matching( quote do __state__ end, __state__)),
 								unquote(some)   ) when unquote(make_guard(__guard__)) do
 				cast_info_return(unquote(body), __nameproc__) 
 			end
+		end
 
+		quote do
+			definfo	unquote(some), state: name do
+				definfo_body(name, Exdk.get(name), unquote(some))
+			end
+			unquote(priv_function_body)
 		end 
 
 	end
@@ -194,12 +217,7 @@ defmodule Sgs.Macro do
 		__guard__ = opts[:when]
 		__reason__ = opts[:reason]
 
-		quote do
-
-			def terminate( reason, nameproc ) do
-				terminate_body( nameproc, Exdk.get(nameproc), reason )
-			end
-
+		priv_function_body = quote do
 			defp terminate_body( 	unquote(do_pattern_matching( quote do __nameproc__ end, __nameproc__ )),
 									unquote(do_pattern_matching( quote do __state__ end, __state__)),
 									unquote(do_pattern_matching( quote do __reason__ end, __reason__))   ) when unquote(make_guard(__guard__)) do
@@ -207,9 +225,30 @@ defmodule Sgs.Macro do
 				# call here, wait for cleanup if it need
 				Sgs.CleanupDaemon.send_terminate_reason( __reason__, __nameproc__ )
 			end
+		end
 
+		quote do
+			def terminate( reason, nameproc ) do
+				terminate_body( nameproc, Exdk.get(nameproc), reason )
+			end
+			unquote(priv_function_body)
 		end
 	end
+
+	#defmacro end_compilation do
+	#	send :pg2.get_members("priv_funcs_writer")
+	#			|> List.first, %{ give_result_to: self }
+	#	
+	#	res = receive do
+	#		%{ quoted_result: collected } -> :pg2.delete("priv_funcs_writer")
+	#										 quote do unquote(collected) end
+	#	end
+	#
+	#	IO.puts Macro.to_string(res)
+	#
+	#	res
+	#
+	#end
 
 	def cleanup_sgs(name) do
 		Exdk.delete(name)
