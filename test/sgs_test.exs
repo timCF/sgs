94b7bfe1bfb119085031dd13e5a95b33ea74b405
tim <tim@tim-macbook.local>
2014-08-29 16:45:08 +0400

    doing tests

--- test/sgs_test.exs
+++ test/sgs_test.exs
@@ -1,11 +1,39 @@
 defmodule SgsTest do
   use ExUnit.Case
 
-  test "1" do
+	defmodule StorageTest do
 
-  	IO.puts 1
+		use Sgs.Macro
+		
+		@timeout :timer.seconds(10)
 
-    assert true
-  end
+		init_sgs state: state = %{nameproc: name, state: num}, nameproc: name, cleanup_reasons: [:my_reason, :normal] do
+			{:ok , state}
+		end
+		init_sgs state: :not_found, nameproc: :my_name, cleanup_reasons: [:my_reason, :normal] do
+			{:ok , %{nameproc: :my_name, state: 1}}
+		end
+		init_sgs state: :not_found, nameproc: name, cleanup_reasons: [:my_reason, :normal] do
+			{:ok , %{nameproc: name, state: 0}}
+		end
+
+		call_sgs kill(reason), state: state = %{} do
+			{:stop, reason, true, state}
+		end
+		call_sgs get_state, state: state = %{} do
+			{:reply, state, state}
+		end
+
+		def test do
+			:supervisor.start_child Sgs.Supervisor, Supervisor.Spec.worker(Sgs.CompileTest, [:my_name], [id: :my_name,  restart: :transient])
+			results = [Exdk.get(:my_name)==%{nameproc: :my_name, state: 0}]
+			results = results++[Sgs.CompileTest.kill(:my_reason)]
+		end
+	end
+
+	test "the truth" do
+		:supervisor.start_child Sgs.Supervisor, Supervisor.Spec.worker(Sgs.CompileTest, [:my_name], [id: :my_name,  restart: :transient])
+		assert :erlang.whereis(:my_name) != :undefined
+	end
   
 end
